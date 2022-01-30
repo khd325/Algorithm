f0d757020319d88c8816f91482047db12ac6598d
khd325 <80850223+khd325@users.noreply.github.com>
2022-01-30 23:01:00 +0900

    [Bronze I] Title: 그대로 출력하기 2, Time: 232 ms, Memory: 17900 KB -BaekjoonHub

---
+++ 백준/Bronze/11719.그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNextLine()){
+            String a = sc.nextLine();
+            System.out.println(a);
+        }
+
+    }
+}
