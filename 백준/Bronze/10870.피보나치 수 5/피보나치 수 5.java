4ec023807fb6a6e71fc8d7f723be1786977d4534
khd325 <80850223+khd325@users.noreply.github.com>
2022-02-06 15:47:29 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 160 ms, Memory: 16140 KB -BaekjoonHub

---
+++ 백준/Bronze/10870.피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    static int[] dp;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        if(n == 0){
+            System.out.println(0);
+            System.exit(0);
+        }else if( n == 1 || n == 2){
+            System.out.println(1);
+            System.exit(0);
+        }
+        dp = new int[n+1];
+        dp[1] = 1; dp[2] =1;
+
+        for(int i = 3; i <= n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        System.out.println(dp[n]);
+
+    }
+}
