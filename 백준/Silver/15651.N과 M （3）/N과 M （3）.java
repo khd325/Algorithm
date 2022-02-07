fb566da88d5d4ffbef1356732390453044cd73c1
khd325 <80850223+khd325@users.noreply.github.com>
2022-02-07 15:30:01 +0900

    [Silver III] Title: N과 M (3), Time: 432 ms, Memory: 67904 KB -BaekjoonHub

--- 백준/Silver/15651.N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651.N과 M （3）/N과 M （3）.java
@@ -1,36 +1,39 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 
 public class Main {
+    static int n;
+    static int m;
+
     static int[] arr;
-    static int n, m;
     static StringBuilder sb = new StringBuilder();
 
-    static void dfs(int at, int depth) {
-        if(depth == m){
-            for(int i = 0; i < arr.length;i++){
-                sb.append(arr[i]).append(" ");
+    static void dfs(int depth) {
+        if (depth == m) {
+            for (int j = 0; j < arr.length; j++) {
+                sb.append(arr[j]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for(int i = 1; i<=n;i++){
-            arr[depth] = i;
-            dfs(i+1,depth+1);
+        for (int j = 1; j <= n; j++) {
+            arr[depth] = j;
+            dfs(depth + 1);
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
         arr = new int[m];
 
-        dfs(1,0);
+        dfs( 0);
         System.out.println(sb);
     }
 }
