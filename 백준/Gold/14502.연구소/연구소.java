f5ee255cc7513312ac23c7bb357dac9bda17613f
khd325 <80850223+khd325@users.noreply.github.com>
2022-02-13 16:02:34 +0900

    [Gold V] Title: 연구소, Time: 740 ms, Memory: 24260 KB -BaekjoonHub

--- 백준/Gold/14502.연구소/연구소.java
+++ 백준/Gold/14502.연구소/연구소.java
@@ -21,88 +21,93 @@ class Node{
 
 
 public class Main {
-    static int n, m;
-    static int[][] map;
-    static int cnt = 0;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static int max = Integer.MIN_VALUE;
 
-    static void bfs(){
-        Queue<Node> q = new LinkedList<>();
-        int[][] clone = new int[n][m];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                clone[i][j] = map[i][j];
-                if(clone[i][j] == 2) q.offer(new Node(i,j));
-            }
-        }
+    public static int n, m, result = 0;
+    public static int[][] arr = new int[8][8]; // 초기 맵 배열
+    public static int[][] temp = new int[8][8]; // 벽을 설치한 뒤의 맵 배열
 
-        while(!q.isEmpty()){
-            Node node = q.poll();
-            int x = node.getX();
-            int y = node.getY();
+    // 4가지 이동 방향에 대한 배열
+    public static int[] dx = {-1, 0, 1, 0};
+    public static int[] dy = {0, 1, 0, -1};
 
-            for(int i = 0; i < 4;i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if(nx <0 || ny < 0 || nx >= n || ny >= m) continue;
-
-                if(clone[nx][ny] ==0){
-                    clone[nx][ny] = 2;
-                    q.offer(new Node(nx,ny));
+    // 깊이 우선 탐색(DFS)을 이용해 각 바이러스가 사방으로 퍼지도록 하기
+    public static void virus(int x, int y) {
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            // 상, 하, 좌, 우 중에서 바이러스가 퍼질 수 있는 경우
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                if (temp[nx][ny] == 0) {
+                    // 해당 위치에 바이러스 배치하고, 다시 재귀적으로 수행
+                    temp[nx][ny] = 2;
+                    virus(nx, ny);
                 }
             }
         }
-        int cnt = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j <m ;j++){
-                if(clone[i][j] == 0){
-                    cnt++;
-                }
-            }
-        }
-        max = Math.max(cnt,max);
     }
 
-    static void check() {
-        if (cnt == 3) {
-            bfs();
+    // 현재 맵에서 안전 영역의 크기 계산하는 메서드
+    public static int getScore() {
+        int score = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (temp[i][j] == 0) {
+                    score += 1;
+                }
+            }
+        }
+        return score;
+    }
+
+    // 깊이 우선 탐색(DFS)을 이용해 울타리를 설치하면서, 매 번 안전 영역의 크기 계산
+    public static void dfs(int count) {
+        // 울타리가 3개 설치된 경우
+        if (count == 3) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    temp[i][j] = arr[i][j];
+                }
+            }
+            // 각 바이러스의 위치에서 전파 진행
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (temp[i][j] == 2) {
+                        virus(i, j);
+                    }
+                }
+            }
+            // 안전 영역의 최대값 계산
+            result = Math.max(result, getScore());
             return;
         }
-
+        // 빈 공간에 울타리를 설치
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (cnt != 3 && map[i][j] == 0) {
-                    map[i][j] = 1;
-                    cnt++;
-                    check();
-                    map[i][j] = 0;
-                    cnt--;
+                if (arr[i][j] == 0) {
+                    arr[i][j] = 1;
+                    count += 1;
+                    dfs(count);
+                    arr[i][j] = 0;
+                    count -= 1;
                 }
             }
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
+        n = sc.nextInt();
+        m = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = sc.nextInt();
             }
         }
-        check();
-        System.out.println(max);
 
+        dfs(0);
+        System.out.println(result);
     }
 }
 
