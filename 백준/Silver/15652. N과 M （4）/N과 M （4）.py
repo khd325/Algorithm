7b002b78c6dc7e262812a212ee746a421d0e33bc
khd325 <80850223+khd325@users.noreply.github.com>
2022-04-28 14:52:57 +0900

    [Silver III] Title: N과 M (4), Time: 84 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,17 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+array = [0] * m
+
+def dfs(start,depth):
+    if depth == m:
+        print(*array)
+        return
+
+    for i in range(start, n+1):
+        array[depth] = i
+        dfs(i,depth+1)
+
+
+dfs(1,0)
