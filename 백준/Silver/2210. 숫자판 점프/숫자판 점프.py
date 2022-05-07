5c8178459d3d46c835a6e64d42bb5073922893d6
khd325 <80850223+khd325@users.noreply.github.com>
2022-05-07 18:28:40 +0900

    [Silver II] Title: 숫자판 점프, Time: 636 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
@@ -3,24 +3,21 @@ import sys
 sys.setrecursionlimit(10 ** 9)
 
 
-def dfs(x, y, depth):
-    if depth == 6:
-        temp = ''.join(array)
-        if temp not in result:
-            result.append(temp)
+def dfs(x, y, l):
+
+    if len(l) == 6:
+        if l not in result:
+            result.append(l)
         return
 
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
         if 0 <= nx < 5 and 0 <= ny < 5:
-            array[depth] = graph[x][y]
-            dfs(nx,ny,depth+1)
-
+            dfs(nx,ny,l + graph[nx][ny])
 
 result = []
 graph = []
-array = [''] * 6
 cnt = 0
 for _ in range(5):
     graph.append(list(map(str, sys.stdin.readline().split())))
@@ -30,6 +27,7 @@ dy = [0, 0, -1, 1]
 
 for i in range(5):
     for j in range(5):
-        dfs(i, j, 0)
+        l = graph[i][j]
+        dfs(i, j, l)
 
 print(len(result))
