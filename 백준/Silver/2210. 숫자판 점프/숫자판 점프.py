f460ccc6ffc353f2319313a6d57f6ee7f5a9afeb
khd325 <80850223+khd325@users.noreply.github.com>
2022-05-07 18:24:51 +0900

    [Silver II] Title: 숫자판 점프, Time: 2016 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
@@ -10,13 +10,12 @@ def dfs(x, y, depth):
             result.append(temp)
         return
 
-    if 0 <= x < 5 and 0 <= y < 5:
-        array[depth] = graph[x][y]
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            dfs(nx, ny, depth + 1)
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < 5 and 0 <= ny < 5:
+            array[depth] = graph[x][y]
+            dfs(nx,ny,depth+1)
 
 
 result = []
