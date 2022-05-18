ef4492f733c03c821a4bfee5a5e454997caa6da1
khd325 <80850223+khd325@users.noreply.github.com>
2022-05-18 16:58:34 +0900

    [Gold IV] Title: LCS 2, Time: 680 ms, Memory: 54140 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -0,0 +1,34 @@
+import sys
+
+a = list(sys.stdin.readline().rstrip())
+b = list(sys.stdin.readline().rstrip())
+
+dp = [[0] * (len(a) + 1) for _ in range(len(b) + 1)]
+
+for i in range(1,len(dp)):
+    for j in range(1,len(dp[i])):
+        if a[j-1] == b[i-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+
+print(dp[-1][-1])
+result = []
+
+i = len(dp) - 1
+j = len(dp[i]) - 1
+
+while dp[i][j] != 0:
+    if dp[i][j] == dp[i-1][j]:
+        i -= 1
+    elif dp[i][j] == dp[i][j-1]:
+        j -= 1
+
+    else:
+        result.append(a[j-1])
+        i -= 1
+        j -= 1
+
+result.reverse()
+
+print(''.join(result))
