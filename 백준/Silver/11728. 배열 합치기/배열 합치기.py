c0230bf5cc0a2ab703a7030491422478f7acd4fa
khd325 <80850223+khd325@users.noreply.github.com>
2022-07-02 20:33:44 +0900

    [Silver V] Title: 배열 합치기, Time: 1492 ms, Memory: 185484 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -0,0 +1,11 @@
+import sys
+
+n, m = map(int,sys.stdin.readline().split())
+
+a = list(map(int,sys.stdin.readline().split()))
+b = list(map(int,sys.stdin.readline().split()))
+
+result = a + b
+result.sort()
+
+print(*result)
