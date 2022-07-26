efed945d45a9f9e969eee20ee2492efb0bed218d
khd325 <80850223+khd325@users.noreply.github.com>
2022-07-26 18:50:53 +0900

    [Bronze I] Title: ROT13, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.py
@@ -0,0 +1,13 @@
+import sys
+
+arr = sys.stdin.readline().rstrip()
+
+for i in arr:
+    if i.islower():
+        print(chr(97 + (ord(i) + 13 - 97) % 26), end='')
+
+    elif i.isupper():
+        print(chr(65 + (ord(i) + 13 - 65) % 26), end='')
+
+    else:
+        print(i, end='')
