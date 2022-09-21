420373f5ee827ed83a28322811806caa71240cf4
khd325 <80850223+khd325@users.noreply.github.com>
2022-09-21 20:59:55 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.46 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public String solution(int n) {
+        String result = "";
+        for(int i = 0; i < n; i++){
+            result += i % 2 == 0 ? "수" : "박";
+        }
+        return result;
+    }
+}
