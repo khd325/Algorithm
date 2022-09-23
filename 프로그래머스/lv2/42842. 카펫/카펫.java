18aaaf2d0a0db06aea3c3a8f5d0b2921df9444ec
khd325 <80850223+khd325@users.noreply.github.com>
2022-09-23 11:01:09 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int area = brown + yellow;
+        
+        for(int i = area-1; i >= 1; i--){
+            if(area % i == 0){
+                int width = i;
+                int height = area / width;
+                
+                if ((width - 2) * (height - 2) == yellow){
+                    int b = area - (width - 2) * (height - 2);
+                    if(b == brown){
+                        answer[0] = width;
+                        answer[1] = height;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return answer;
+        
+    }
+}
