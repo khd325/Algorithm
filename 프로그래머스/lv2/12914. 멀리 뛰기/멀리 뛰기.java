e80df78a5928275739e91b6c7a825805d1d58c6b
khd325 <80850223+khd325@users.noreply.github.com>
2022-09-23 11:39:11 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(int n) {
+        long[] d = new long[2001];
+        d[1] = 1;
+        d[2] = 2;
+        
+        for(int i = 3; i <= n; i++){
+            d[i] = (d[i-1] + d[i-2]) % 1234567;
+        }
+        return d[n];
+
+    }
+}
