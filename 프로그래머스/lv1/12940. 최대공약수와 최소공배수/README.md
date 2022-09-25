5c4bb24d1f74538032953eac1704d5b6efc1bf1c
khd325 <80850223+khd325@users.noreply.github.com>
2022-09-25 23:20:24 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/README.md
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최대공약수와 최소공배수 - 12940 
 
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/12940?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.00 ms
+메모리: 77.4 MB, 시간: 0.01 ms
 
 ### 구분
 
---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,17 @@
+class Solution {
+    static int gcd(int n, int m){
+        if(m==0) return n;
+        
+        return gcd(m, n % m);
+    }
+    
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        
+        int gcd = gcd(n,m);
+        answer[0] = gcd;
+        answer[1] = (n * m / gcd);
+        
+        return answer;
+    }
+}
