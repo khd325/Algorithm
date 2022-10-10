ace493c90db0cdfa41433c39d52ccbd5db4df98b
khd325 <80850223+khd325@users.noreply.github.com>
2022-10-10 19:47:12 +0900

    [unrated] Title: 개미 군단, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int hp) {
+        
+        int cnt = 0;
+        
+        cnt += hp / 5;
+        hp %= 5;
+        
+        cnt += hp / 3;
+        hp %= 3;
+        
+        cnt += hp / 1;
+        
+        
+        return cnt;
+        
+    }
+}
