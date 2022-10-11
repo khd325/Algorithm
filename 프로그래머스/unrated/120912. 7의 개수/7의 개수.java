fdb3104d5ec3a15108ffef3c49a9a240b5c0c284
khd325 <80850223+khd325@users.noreply.github.com>
2022-10-11 21:17:59 +0900

    [unrated] Title: 7의 개수, Time: 0.06 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        for(int i = 0; i < array.length; i++){
+            String num = String.valueOf(array[i]);
+            for(int j = 0; j < num.length(); j++){
+                if(num.charAt(j) - '0' == 7){
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
