e3cd74d00d4795746ffa36789d8a844d8f0f9b77
khd325 <80850223+khd325@users.noreply.github.com>
2022-10-12 13:45:02 +0900

    [unrated] Title: 합성수 찾기, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n) {
+        int result = 0;
+        
+        for(int i = 4; i <= n; i++){
+            int count = 0;
+            for(int j = 1; j <= i; j++){
+                if(i % j == 0){
+                    count++;
+                }
+            }
+            
+            if(count >= 3){
+                result++;
+            }
+        }
+            
+        return result;
+    }
+}
