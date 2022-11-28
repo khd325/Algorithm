cb47b98693ffe3d1678a50a96d5c4e560d3f2998
khd325 <80850223+khd325@users.noreply.github.com>
2022-11-28 22:01:26 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,16 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int round = 0;
+                
+        while(a != b) {
+            round++;
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+        }
+        
+        return round;
+    
+    }
+}
