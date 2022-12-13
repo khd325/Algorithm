42b1882a9bceee618fefe3202d20bb9a8a6d07d7
khd325 <80850223+khd325@users.noreply.github.com>
2022-12-13 20:57:34 +0900

    [unrated] Title: 옹알이 (2), Time: 3.07 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,36 +1,6 @@
-import java.util.*;
 class Solution {
     public int solution(String[] babbling) {
-        ArrayList<String> arr = new ArrayList<>();
-        for (int i = 0; i < babbling.length; i++) {
-            babbling[i] = babbling[i].replaceAll("aya", "A");
-            babbling[i] = babbling[i].replaceAll("ye", "B");
-            babbling[i] = babbling[i].replaceAll("woo", "C");
-            babbling[i] = babbling[i].replaceAll("ma", "D");
-        }
-
-        for (int i = 0; i < babbling.length; i++) {
-            boolean isDuplicate = false;
-            for (int j = 0; j < babbling[i].length() - 1; j++) {
-                if(babbling[i].charAt(j) == babbling[i].charAt(j+1)){
-                    isDuplicate = true;
-                }
-            }
-            if(isDuplicate) continue;
-            
-            
-            boolean isUpper = true;
-            for (int j = 0; j < babbling[i].length(); j++) {
-                if (!Character.isUpperCase(babbling[i].charAt(j))) {
-                    isUpper = false;
-                    break;
-                }
-            }
-            if (isUpper) {
-                arr.add(babbling[i]);
-            }
-        }
-
-        return arr.size();
+        int answer = 0;
+        return answer;
     }
 }
