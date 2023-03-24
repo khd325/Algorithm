29647618ab61054b773e083b69360d52161337f5
khd325 <80850223+khd325@users.noreply.github.com>
2023-03-24 20:16:40 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT concat('/home/grep/src/', BOARD_ID, '/', FILE_ID,FILE_NAME, FILE_EXT) as FILE_PATH from USED_GOODS_FILE where BOARD_ID = (select BOARD_ID from USED_GOODs_BOARD order by views desc limit 1) order by FILE_ID desc;
