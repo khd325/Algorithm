997796ac7aa9189913911c711a17d859712c7d81
khd325 <80850223+khd325@users.noreply.github.com>
2023-04-10 15:31:09 +0900

    [Silver IV] Title: 수 찾기, Time: 596 ms, Memory: 43148 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -5,54 +5,46 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean search(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length - 1;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
 
-            if(arr[mid] == target) {
-                return true;
-            }
+        while (start <= end) {
+            int mid = (start + end) / 2;
 
-            if(arr[mid] > target) {
-                right = mid - 1;
-            }else {
-                left = mid + 1;
+            if (arr[mid] == target) {
+                return 1;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
             }
         }
-        return false;
+        return 0;
+
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[N];
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(arr);
-
-        int M = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < M; i++) {
-            boolean isExist = search(arr, Integer.parseInt(st.nextToken()));
-            if(isExist) {
-                sb.append(1).append("\n");
-            }else {
-                sb.append(0).append("\n");
-            }
+        for (int i = 0; i < m; i++) {
+            sb.append(binarySearch(arr, Integer.parseInt(st.nextToken()))).append("\n");
         }
 
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
