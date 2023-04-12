92aaa8b33d7af93b5945ea639f37a0c685e3b1fe
khd325 <80850223+khd325@users.noreply.github.com>
2023-04-12 11:47:41 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static void reverse(int a, int b, int[] arr) {
+        for (int i = a, j = b; i < j; i++, j--) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+
+            reverse(a, b, arr);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : arr) {
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
