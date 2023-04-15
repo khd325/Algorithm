17c9ddcab6df4a7bf97a44d9098769560f5de1bc
khd325 <80850223+khd325@users.noreply.github.com>
2023-04-15 19:00:17 +0900

    [Gold IV] Title: 서강그라운드, Time: 132 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.java
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.java
@@ -1,10 +1,75 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static class Node implements Comparable<Node> {
+        int index;
+        int dist;
+
+        public Node(int index, int dist) {
+            this.index = index;
+            this.dist = dist;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public int getDist() {
+            return dist;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return dist - o.getDist();
+        }
+    }
+
+    static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+    static int[] items;
+
+    static int dijkstra(int start, int m) {
+        int[] distance = new int[graph.size()];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
+        distance[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            int now = node.getIndex();
+            int dist = node.getDist();
+
+            if (distance[now] < dist) continue;
+
+            for (int i = 0; i < graph.get(now).size(); i++) {
+                Node temp = graph.get(now).get(i);
+                int nextIndex = temp.getIndex();
+                int nextDist = temp.getDist();
+
+                if (distance[nextIndex] > distance[now] + nextDist) {
+                    distance[nextIndex] = distance[now] + nextDist;
+                    pq.add(new Node(nextIndex, distance[nextIndex]));
+                }
+            }
+        }
+
+        int sum = 0;
+        for (int i = 1; i < distance.length; i++) {
+            if(distance[i] <= m) {
+                sum += items[i];
+            }
+        }
+
+        return sum;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,14 +79,11 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
         int r = Integer.parseInt(st.nextToken());
 
-
-        int[] items = new int[n + 1];
-        int[][] graph = new int[n + 1][n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            Arrays.fill(graph[i], (int) 1e9);
-            graph[i][i] = 0;
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
         }
+
+        items = new int[n + 1];
         st = new StringTokenizer(br.readLine());
 
         for (int i = 1; i <= n; i++) {
@@ -30,34 +92,19 @@ public class Main {
 
         for (int i = 0; i < r; i++) {
             st = new StringTokenizer(br.readLine());
+
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            graph[a][b] = c;
-            graph[b][a] = c;
+            graph.get(a).add(new Node(b, c));
+            graph.get(b).add(new Node(a, c));
         }
 
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
-                }
-            }
-        }
         int result = 0;
-
-        for(int i =1; i <= n; i++){
-            int temp = 0;
-            for(int j = 1; j <= n; j++){
-                if(graph[i][j] <= m) {
-                    temp += items[j];
-                }
-            }
-
-            result = Math.max(result,temp);
+        for (int i = 1; i <= n; i++) {
+            result = Math.max(dijkstra(i,m),result);
         }
-
         System.out.println(result);
     }
 }
