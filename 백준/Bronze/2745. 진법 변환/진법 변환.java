d18e1de80111067f0432a68573570a2e3953512a
khd325 <80850223+khd325@users.noreply.github.com>
2023-04-24 21:04:43 +0900

    [Bronze II] Title: 진법 변환, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -9,7 +9,24 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        System.out.println(Integer.parseInt(st.nextToken(), Integer.parseInt(st.nextToken())));
+        String str = st.nextToken();
+        int radix = Integer.parseInt(st.nextToken());
+
+        int power = 1;
+
+        int result = 0;
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+
+            if (Character.isAlphabetic(str.charAt(i))) {
+                result += (str.charAt(i) - 'A' + 10) * power;
+            } else {
+                result += (str.charAt(i) - '0') * power;
+            }
+            power *= radix;
+        }
+
+        System.out.println(result);
 
     }
 }
