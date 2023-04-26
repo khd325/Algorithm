bd8136652347ed41fa7e8e5a0299c538c8facca3
khd325 <80850223+khd325@users.noreply.github.com>
2023-04-26 16:22:43 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 356 ms, Memory: 30772 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] = arr[i][j - 1] + Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int k = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int sum = 0;
+
+            for (int u = x1; u <= x2; u++) {
+                if (y1 == 1) {
+                    sum += arr[u][y2];
+                } else {
+                    sum += arr[u][y2] - arr[u][y1-1];
+                }
+            }
+            sb.append(sum).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+}
